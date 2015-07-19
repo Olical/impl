3294aac6f17757307021ede9a3321f7a6a3420bb
Oliver Caldwell <olliec87@gmail.com>
2015-07-19 11:10:14 +0100

    Renamed a test to keep it in line

--- test/read.js
+++ test/read.js
@@ -7,7 +7,7 @@ test('reading symbols and numbers', function (t) {
   t.deepEqual(out, [new impl.Symbol('+'), 10, 20], '"+" symbol with two numbers')
 })
 
-test('reading a string', function (t) {
+test('reading symbol and string', function (t) {
   t.plan(1)
   var out = impl.read('split "Hello, World!"').toJS()
   t.deepEqual(out, [new impl.Symbol('split'), 'Hello, World!'], '"split" symbol with a string')
