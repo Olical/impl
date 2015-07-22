baa0f8fa2f35a08fa3f5ddbf35f7b98e19c3999c
Oliver Caldwell <olliec87@gmail.com>
2015-07-22 23:08:14 +0100

    New reader, can understand strings

---
+++ src/read.js
@@ -0,0 +1,84 @@
+var _ = require('lodash')
+var Immutable = require('immutable')
+// var Symbol = require('./Symbol')
+
+var config = {
+  escapeChar: '\\',
+  itemDelimiterChar: ' ',
+  stringChar: '"'
+}
+
+/**
+ * Reads a Impl source string into it's equivalent data structures.
+ *
+ * @param {String} source
+ * @return {Immutable.List}
+ */
+function read (source) {
+  var cursor = 0
+  var sourceLength = source.length
+  var result = Immutable.List()
+  var readerResult
+
+  while (cursor < sourceLength) {
+    readerResult = selectReader(source[cursor])(source, cursor)
+    cursor = readerResult.cursor
+
+    if (readerResult.value !== null) {
+      result = result.push(readerResult.value)
+    }
+  }
+
+  return result
+}
+
+/**
+ * Fetches a reader function for the given character. The reader function
+ * requires the source and cursor position, it returns an object containing the
+ * read value and the new cursor position.
+ *
+ * @param {String} char
+ * @return {Function}
+ */
+function selectReader (char) {
+  if (char === config.escapeChar) {
+    return readers.skip(2)
+  } else if (char === config.stringChar) {
+    return readers.string()
+  } else if (char === config.itemDelimiterChar) {
+    return readers.skip(1)
+  } else {
+    throw new Error(['Unknown character found when attempting to select a reader: "', char, '".'].join(''))
+  }
+}
+
+var readers = {
+  skip: function (n) {
+    return function (source, cursor) {
+      return {
+        value: null,
+        cursor: cursor + n
+      }
+    }
+  },
+  string: function () {
+    return function (source, cursor) {
+      source = _.drop(source, cursor + 1)
+      var prev
+      var shouldTake
+
+      var str = _.takeWhile(source, function (char) {
+        shouldTake = char !== config.stringChar && prev !== config.escapeChar
+        prev = char
+        return shouldTake
+      })
+
+      return {
+        value: str.join(''),
+        cursor: cursor + str.length + 2
+      }
+    }
+  }
+}
+
+module.exports = read
--- test/read.js
+++ test/read.js
@@ -1,14 +1,26 @@
 var impl = require('..')
 var test = require('tape')
 
-test('reading symbols and numbers', function (t) {
+test('reading a string', function (t) {
   t.plan(1)
-  var out = impl.read('+ 10 20').toJS()
-  t.deepEqual(out, [new impl.Symbol('+'), 10, 20], '"+" symbol with two numbers')
+  var out = impl.read('"Hello, World!"').toJS()
+  t.deepEqual(out, ['Hello, World!'], 'just a string')
 })
 
-test('reading symbol and string', function (t) {
+test('reading two strings', function (t) {
   t.plan(1)
-  var out = impl.read('split "Hello, World!"').toJS()
-  t.deepEqual(out, [new impl.Symbol('split'), 'Hello, World!'], '"split" symbol with a string')
+  var out = impl.read('"Hello" "World!"').toJS()
+  t.deepEqual(out, ['Hello', 'World!'], 'a couple of strings')
 })
+
+// test('reading symbols and numbers', function (t) {
+//   t.plan(1)
+//   var out = impl.read('+ 1.0 -20 6').toJS()
+//   t.deepEqual(out, [new impl.Symbol('+'), 1.0, -20, 6], '"+" symbol with various numbers')
+// })
+
+// test('reading symbol and string', function (t) {
+//   t.plan(1)
+//   var out = impl.read('split "Hello, World!"').toJS()
+//   t.deepEqual(out, [new impl.Symbol('split'), 'Hello, World!'], '"split" symbol with a string')
+// })
